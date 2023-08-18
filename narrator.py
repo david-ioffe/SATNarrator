32829dc60d236a717a4bad4e961ae12016e0d71f
david-ioffe <140282082+david-ioffe@users.noreply.github.com>
2023-08-18 19:27:13 +0300

    Update narrator.py

--- narrator.py
+++ narrator.py
@@ -1,23 +1,11 @@
 import pyttsx3
 import time 
 
-# Initialize the text-to-speech engine
 engine = pyttsx3.init()
 
-# # Set the text you want to convert to speech in Hebrew
-# "Naa Laabbuor La Perreck Habba"
-# text = "Naa Laabbuor La Perreck Habba"
-
-# # Set the Hebrew language voice
-# engine.setProperty('voice', 'he')
-
-# # Convert text to speech
-# engine.say(text)
-# engine.runAndWait()
-
 def start_test(chapters=8, time_for_each_chapter=20, breaks=False):
-    first_period = time_for_each_chapter * (3/4)# * 60
-    second_period = time_for_each_chapter * (1/4)# * 60
+    first_period = time_for_each_chapter * (3/4) * 60
+    second_period = time_for_each_chapter * (1/4) * 60
     for period in range(chapters):
         engine.say(f"Chapter {period + 1} Started")
         engine.runAndWait()
