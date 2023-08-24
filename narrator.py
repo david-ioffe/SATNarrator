fc86a50b7f2c7a9e6f5d7670d4c5f3e66bd2dcaf
David-hosting <david.ioffe@mail.huji.ac.il>
2023-08-24 13:07:27 +0300

    Update narrator.py
    Added countdown before start & added a text chapter.

--- narrator.py
+++ narrator.py
@@ -3,11 +3,34 @@ import time
 
 engine = pyttsx3.init()
 
-def start_test(chapters=8, time_for_each_chapter=20, breaks=False):
+def start_countdown(seconds : int) -> None:
+    for second in (seconds, 0, -1):
+        engine.say(f"{second} seconds untill start.")
+        engine.runAndWait()
+        time.sleep(1)
+    engine.say("Exam started.")
+    engine.runAndWait()
+    start_test(chapters=8,time_for_each_chapter=20,breaks=False, text_chapter=True)
+
+def start_text_chapter(time : int) -> None:
+    first_period = time * (5/6) * 60
+    second_period = time * (1/5) * 60
+    engine.say("Chapter Started.")
+    engine.runAndWait()
+    time.sleep(first_period)
+    engine.say(f"{int(second_period)} minutes left!")
+    engine.runAndWait()
+    time.sleep(second_period)
+    engine.say("Time ended, please move to the next chapter!")
+    engine.runAndWait()
+
+def start_test(chapters : int, time_for_each_chapter : int, breaks : bool, text_chapter : bool) -> None:
+    if text_chapter:
+        start_text_chapter()
     first_period = time_for_each_chapter * (3/4) * 60
     second_period = time_for_each_chapter * (1/4) * 60
     for period in range(chapters):
-        engine.say(f"Chapter {period + 1} Started")
+        engine.say(f"Chapter {period + 1} Started.")
         engine.runAndWait()
         time.sleep(first_period)
         engine.say(f"{int(second_period)} minutes left!")
@@ -21,4 +44,12 @@ def start_test(chapters=8, time_for_each_chapter=20, breaks=False):
         engine.runAndWait()
 
 if __name__ == "__main__":
-    start_test(chapters=8,time_for_each_chapter=20,breaks=False)
+    while type(countdown:=input("Enter time for countdown: ")) != int:
+        try: 
+            countdown = int(countdown)
+            break
+        except ValueError:
+            print("", end="")
+    print(countdown)
+
+    start_countdown(countdown)
